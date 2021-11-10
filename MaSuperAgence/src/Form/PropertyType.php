3430ffc9ee5445127d179a02a6182e3e2b0be6d2
dremaux <ldremaux@btslivh.eu>
2021-11-10 14:25:30 +0100

    select2 fonctionne ENFIN !!!!

--- MaSuperAgence/src/Form/PropertyType.php
+++ MaSuperAgence/src/Form/PropertyType.php
@@ -11,6 +11,7 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 
 use Doctrine\DBAL\Query\QueryBuilder;
 use Doctrine\ORM\QueryBuilder as ORMQueryBuilder;
+use PhpParser\Parser\Multiple;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 
@@ -32,7 +33,7 @@ class PropertyType extends AbstractType
             ->add('options', EntityType::class, [
                 'class' => Option::class,
                 'choice_label' => 'name',
-                'multiple' => true,
+                'multiple' => 'multiple',
                 'required' => false,
             ])
 
